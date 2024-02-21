76a0de855834a0c3221b83e1b8f3290a8c81d61a
Andry Ariadi <andryariadi23@gmail.com>
2024-02-21 15:51:21 +0700

    setup pemanggilan api crud

--- crud/src/libs/database/data.js
+++ crud/src/libs/database/data.js
@@ -32,3 +32,44 @@ export const getProductBySlug = async (slug) => {
 
   return res.json();
 };
+
+export const addProduct = async (productData) => {
+  const res = await fetch("http://localhost:3000/api/product", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(productData),
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Failed to add product!");
+
+  return res.json();
+};
+
+export const deleteProduct = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/product/${slug}`, {
+    method: "DELETE",
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Failed to delete product!");
+
+  return res.json();
+};
+
+export const updateProduct = async (slug, productData) => {
+  const res = await fetch(`http://localhost:3000/api/product/${slug}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(productData),
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Failed to update product!");
+
+  return res.json();
+};
